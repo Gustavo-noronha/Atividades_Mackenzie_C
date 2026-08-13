664fef2f9fc9b369beb2bd62a4c963e1f16d2413
Gustavo-noronha <10769664@mackenzista.com.br>
2026-08-13 16:59:57 -0300

    ex1

--- ex1.c
+++ ex1.c
@@ -2,8 +2,11 @@
 
 int main(){
     int idade;
-    printf('qual sua idade ?:');
-    scanf('%d',&idade);
-    printf('Sua idade é %d anos',idade);
+    char nome[50];
+    printf("qual sua idade ?:");
+    scanf("%d",&idade);
+    printf("Qual seu nome?: ");
+    scanf("%s",nome);
+    printf("Sue nome e %s de idade %d anos",nome,idade);
     return 0;
 }
