606886dacabd6fdf6a21c32256af44f9ebde49cd
Gustavo-noronha <10769664@mackenzista.com.br>
2026-08-14 20:02:24 -0300

    Lab1

--- Lab1/exemplo1.c.c
+++ Lab1/exemplo1.c.c
@@ -2,9 +2,9 @@
 
 int main(){
 	printf("Mackenzie - FCI \n");
+	printf("Fundada em no Ano: 1970.");
 	printf("Turma 02N11\n");
 	printf("Algoritmos e ");
 	printf("Prog 2\n");
-	system("pause");
     return 0;
 }
