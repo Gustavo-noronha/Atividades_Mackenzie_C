ace3cc2c6f8d377bf431c365ae399c9e8608fe62
Gustavo-noronha <10769664@mackenzista.com.br>
2026-08-18 22:14:54 -0300

    Update Ex04.cpp

--- Lab1/Ex04.cpp
+++ Lab1/Ex04.cpp
@@ -5,12 +5,12 @@ int main(){
     dinheiro=10000000;
     float salario_min,dolar,quantidade_casas;
     printf("Qual o salario min?: ");
-    scanf("f%",&salario_min);
+    scanf("%f",&salario_min);
     printf("E o valor do dolar?: ");
-    scanf("f%",&dolar);
+    scanf("%f",&dolar);
     dinheiro_d=dinheiro*dolar;
     valor_casa=150*salario_min;
     quantidade_casas=dinheiro_d/valor_casa;
-    printf("A quantidade de casas: f%",quantidade_casas);
+    printf("A quantidade de casas: %f",quantidade_casas);
     return 0;
 }
